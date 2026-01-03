bca2465f16b6ac744ee16996bef87f3abe4aa8df
Kumari Sonika <kumarisonika1929@gmail.com>
2026-01-03 23:34:36 +0530

    event and user

--- src/main/java/org/example/Event.java
+++ src/main/java/org/example/Event.java
@@ -1,4 +1,17 @@
 package org.example;
 
 public class Event {
+    int eventId;
+    String name;
+    String dateTime;
+    String venue;
+    int seats;
+
+    Event(int eventId, String name, String dateTime, String venue, int seats){
+        this.eventId= eventId;
+        this.name=name;
+        this.dateTime=dateTime;
+        this.venue=venue;
+        this.seats=seats;
+    }
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,19 @@
 package org.example;
 
 public class User {
+    int userId;
+    String name;
+
+    User(int userId, String name){
+        this.userId=userId;
+        this.name=name;
+    }
+
+    int getUserId(){
+        return userId;
+    }
+
+    String getName(){
+        return name;
+    }
 }
